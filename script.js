2dbe0639c00be3d6e66d9c75b31eb8fd3ae9b4a7
Shubham <shm.code1@gmail.com>
2023-12-01 00:23:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let map,mapEvent;
+class App{
+    constructor(){
+        this._getPosition();
+    }
+    _getPosition(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),function(){
+                alert('could not get your location')
+            });
+            }
+    }
+    _loadMap(position){
+            // console.log(position);
+            const {latitude}=position.coords;
+            const {longitude}=position.coords;
+            console.log(`https://www.google.com/maps/@${latitude},${longitude},11z?entry=ttu`);
+        
+            const coords=[latitude,longitude]
+            console.log(latitude,longitude);
+            this.map = L.map('map').setView(coords,16);
+            console.log(map);
+        
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+        
+            this.map.on('click',function(mapEv){
+                this.mapEvent=mapEv;
+                form.classList.remove('hidden');
+                inputDistance.focus();
+            });
+        }
+    _showForm(){}
+
+}
+const app=new App();
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+        L.marker([lat,lng]).addTo(map)
+    .bindPopup()
+    .setPopupContent('Workout')
+    .openPopup()
+})
